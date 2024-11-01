fe181735e787e6dc1ea5f36f1233ab8f67c1c086
Justin Mohabir <justinmohabir23@gmail.com>
2024-10-31 20:42:31 -0400

    adding sketch

---
+++ lab4/lab4/lab4.ino
@@ -0,0 +1,35 @@
+
+
+unsigned long currentTime = 0;
+unsigned long lastTime = 0;
+const unsigned long intervalBetweenData = 1000;
+
+
+void setup() {
+  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(500000);
+}
+
+// The loop() function runs over and over so whatever is contained
+// therein will be repeated until while the arduino is on. 
+
+void loop() {
+  
+  // The function analogRead() will
+    
+  int sensorValue = analogRead(A0);
+  
+  // get the current time in microseconds
+  currentTime = micros();
+
+  
+  if (currentTime - lastTime >= intervalBetweenData ) {
+    int sensorValue = analogRead(A0);
+    lastTime+=intervalBetweenData;
+    //time in milliseconds
+    Serial.print(currentTime/1000);
+    Serial.print(',');
+    Serial.println(sensorValue);
+   }
+ }
